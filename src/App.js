887e27771b2470ae7f1af0b6ec472d0abce7b480
HMP1993 <mr.momen.mrm@gmail.com>
2023-02-16 21:09:54 +0000

    curr weather compadded halfway throuth fetching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import Search from './component/search/search';
+import "./App.css";
+import Search from "./component/Current-weather/search/search";
+import CurrentWeather from "./component/Current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
   const hanldeOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    ); 
+  };
 
   return (
     <div className="container">
       <Search onSearchChange={hanldeOnSearchChange} />
+      <CurrentWeather />
     </div>
   );
 }
--- src/component/Current-weather/search/search.jsx
+++ src/component/Current-weather/search/search.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from "../../api";
+import { GEO_API_URL, geoApiOptions } from "../../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
-  
+
   const loadOptions = (inputValue) => {
     return fetch(
       `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
